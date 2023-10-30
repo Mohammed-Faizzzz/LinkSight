99150de52988bed242684d29d944b11f0299c862
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-10-30 18:30:21 +0800

    Update details.html

--- popup.js
+++ popup.js
@@ -1,5 +1,18 @@
-// Fetch the data
+// Fetch the data from chrome.storage.local
+chrome.storage.local.get(null, (results) => {
+    let siteCount = Object.keys(results).length; // Number of unique sites visited
+    let linkCount = 0;
+
+    for (let site in results) {
+        linkCount += results[site].length; // Sum of all hyperlinks across sites
+    }
+
+    document.getElementById('siteCount').textContent = siteCount;
+    document.getElementById('linkCount').textContent = linkCount;
+});
+
 document.getElementById('detailsBtn').addEventListener('click', () => {
     // Open a new tab to display details
     chrome.tabs.create({ url: 'details.html' });
 });
+
