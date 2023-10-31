089148fc506bf74f74ed153eddc20a375fd0bfe1
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-10-31 18:09:29 +0800

    Update details.js and details.html

--- details.js
+++ details.js
@@ -1,12 +1,45 @@
 chrome.storage.local.get(null, (results) => {
-    let tableBody = document.querySelector("#detailsTable tbody");
+    let table = document.createElement('table');
+    
+    // Headers
+    let thead = table.createTHead();
+    let headers = ['Visited Sites', 'Hyperlinks', 'Occurrences'];
+    let headerRow = thead.insertRow();
+    for (let header of headers) {
+        let th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+    }
+
+    // Data Rows
     for (let site in results) {
         if (site === 'isTrackingPaused') continue;
-        let row = tableBody.insertRow();
-        let siteCell = row.insertCell(0);
-        let linkCountCell = row.insertCell(1);
-        
-        siteCell.textContent = site;
-        linkCountCell.textContent = results[site].length;
+        if (Array.isArray(results[site])) {
+            let linksForSite = results[site];
+            for (let link of linksForSite) {
+                let row = table.insertRow();
+
+                let siteCell = row.insertCell();
+                let aSite = document.createElement('a');
+                aSite.href = "https://" + site; 
+                aSite.textContent = site;
+                aSite.target = "_blank"; 
+                siteCell.appendChild(aSite);
+
+                let linkCell = row.insertCell();
+                let aLink = document.createElement('a');
+                aLink.href = link.url;
+                aLink.textContent = link.url;
+                aLink.target = "_blank";
+                linkCell.appendChild(aLink);
+
+                let countCell = row.insertCell();
+                countCell.textContent = link.count;
+            }
+        }
     }
+
+    // Append table to the container
+    document.getElementById('tableContainer').appendChild(table);
 });
+
